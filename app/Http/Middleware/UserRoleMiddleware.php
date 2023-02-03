fdaab6e446511c0c7397827be8a4d080d9e08464
HtetSuMoe-cgm <htetsumoe@cgm-myanmar.com>
2023-02-03 11:26:27 +0630

    fix middleware error

--- app/Http/Middleware/UserRoleMiddleware.php
+++ app/Http/Middleware/UserRoleMiddleware.php
@@ -4,8 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Request;
 
 class UserRoleMiddleware
 {
@@ -22,7 +21,6 @@ class UserRoleMiddleware
         {
             return $next($request);
         }
-        //return back();
         return response()->json(['You do not have permission to access for this page.']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Post\PostController;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +28,7 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/forget-password', [AuthController::class, 'sendEmail'])->name('user.sendEmail');
     Route::get('resetPassword/{token}', [AuthController::class, 'resetPasswordForm'])->name('user.resetPasswordForm.show');
     Route::post('resetPassword', [AuthController::class, 'resetPassword'])->name('user.resetPassword');
-    // Route::get('/detailPost/{post_id}', [PostController::class, 'DetailPost'])->name('detailPost');
+    Route::get('/detailPost/{post_id}', [PostController::class, 'DetailPost'])->name('detailPost');
 });
 
 //Route User
@@ -37,82 +36,15 @@ Route::middleware(['auth', 'user-role:user'])->group(function () {
     //Route::get('/user/home', [PostController::class, 'Home'])->name('home')->middleware(isAuthMiddleware::class);
 });
 
-// Route::group(['middleware' => ['auth', 'user-role:user']], function () {
-
-// });
-
 //Route Admin
 Route::middleware(['auth', 'user-role:admin'])->group(function () {
     Route::get('welcome', [UserController::class, 'Welcome'])->name('welcome');
-    Route::get('/detailPost/{post_id}', [PostController::class, 'DetailPost'])->name('detailPost');
+    Route::get('/admin/user/list', [UserController::class, 'userList'])->name('userList.show');
+    Route::get('/admin/user/create', [UserController::class, 'createUserForm'])->name('createUser.show');
 });
-// Route::group(['middleware' => ['auth', 'user-role:admin']], function () {
-//     Route::get('welcome', [UserController::class, 'Welcome'])->name('welcome')
-//     ->middleware(isAuthMiddleware::class);
-//     Route::get('/detailPost/{post_id}', [PostController::class, 'DetailPost'])->name('detailPost')
-//     ->middleware(isAuthMiddleware::class);
-// });
 
 Route::middleware(['isAuth'])->group(function () {
     Route::group(['prefix' => 'user'], function () {
         Route::get('/', [AuthController::class, 'logout'])->name('logout.perform');
     });
 });
-
-// Route::group(['namespace' => 'App\Http\Controllers'], function()
-// {   
-//     /**
-//      * Home Routes
-//      */
-//     Route::get('/', [PostController::class, 'Home'])->name('home');
-
-//     Route::group(['middleware' => ['guest']], function() {
-//         /**
-//          * Register Routes
-//          */
-//         Route::get('/register', [AuthController::class, 'ViewRegister'])->name('register.show');
-
-//         /**
-//          * Login Routes
-//          */
-//         Route::get('/login', [AuthController::class, 'ViewLogin'])->name('login.show');
-//         Route::post('/login', [AuthController::class, 'UserLogin'])->name('login.perform');
-
-//     });
-
-//     Route::group(['middleware' => ['auth', 'permission']], function() {
-//         /**
-//          * Logout Routes
-//          */
-//         Route::get('/', [AuthController::class, 'Logout'])->name('logout.perform');
-
-//         /**
-//          * User Routes
-//          */
-//         Route::group(['prefix' => 'users'], function() {
-//             // Route::get('/', 'UsersController@index')->name('users.index');
-//             // Route::get('/create', 'UsersController@create')->name('users.create');
-//             // Route::post('/create', 'UsersController@store')->name('users.store');
-//             // Route::get('/{user}/show', 'UsersController@show')->name('users.show');
-//             // Route::get('/{user}/edit', 'UsersController@edit')->name('users.edit');
-//             // Route::patch('/{user}/update', 'UsersController@update')->name('users.update');
-//             // Route::delete('/{user}/delete', 'UsersController@destroy')->name('users.destroy');
-//         });
-
-//         /**
-//          * User Routes
-//          */
-//         Route::group(['prefix' => 'posts'], function() {
-//             Route::get('/', 'PostsController@index')->name('posts.index');
-//             Route::get('/create', 'PostsController@create')->name('posts.create');
-//             Route::post('/create', 'PostsController@store')->name('posts.store');
-//             Route::get('/{post}/show', 'PostsController@show')->name('posts.show');
-//             Route::get('/{post}/edit', 'PostsController@edit')->name('posts.edit');
-//             Route::patch('/{post}/update', 'PostsController@update')->name('posts.update');
-//             Route::delete('/{post}/delete', 'PostsController@destroy')->name('posts.destroy');
-//         });
-
-//         Route::resource('roles', RolesController::class);
-//         Route::resource('permissions', PermissionsController::class);
-//     });
-// });
