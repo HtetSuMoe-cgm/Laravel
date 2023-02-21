7fdf5060f91bd383d24f32a58a7c51f8e7320712
HtetSuMoe-cgm <htetsumoe@cgm-myanmar.com>
2023-02-21 09:50:17 +0630

    modify datatable

--- public/js/dataTable.js
+++ public/js/dataTable.js
@@ -1,9 +1,13 @@
-
-    $(document).ready(function() {
-        $('#data-table').DataTable({
-            "paging": true,
-            "pageLength": 5,
-            "bLengthChange": false,
-        });
+$(document).ready(function() {
+    $('#data-table').DataTable({
+        "paging": true,
+        "pageLength": 5,
+        "bLengthChange": false,
+        "bFilter": true,
+        "bInfo": false,
+        "bAutoWidth": false,
+        "dom": 'rtp',
+        "scrollX": false,
+        "responsive": true
     });
-
+});
